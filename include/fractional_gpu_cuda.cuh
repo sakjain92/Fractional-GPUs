35692c7778433e987935538aac4c840ddcac56df
Saksham Jain <sakjain92@gmail.com>
2018-07-23 12:32:31 -0400

    Reduce latency of launching kernels
    To eliminate the possibility of livelock (in the compute partitioning
    algo), only one kernel is launched at a time and before launching, it
    waits for all the persistent blocks of the previous launch to be
    completed. This wait time has been reduced.
    
    Signed-off-by: Saksham Jain <sakjain92@gmail.com>

--- include/fractional_gpu_cuda.cuh
+++ include/fractional_gpu_cuda.cuh
@@ -23,9 +23,10 @@ int fgpu_device_init(const fgpu_dev_ctx_t *dev_ctx)
     uint sm;
     asm("mov.u32 %0, %smid;" : "=r"(sm));
     if (threadIdx.x == 0 && threadIdx.y == 0 && threadIdx.z == 0) {
-       
-        /* Prepare for the next function */
-        dev_ctx->d_bindex->bindexes[dev_ctx->color].index[dev_ctx->index ^ 1] = 0;
+
+        /* Note: This is the most time taking process */
+        dev_ctx->d_host_indicators->indicators[blockIdx.x].started = true;
+
         dev_ctx->d_dev_indicators->indicators[blockIdx.x].started = true;
 
         /* Pblocks launched on wrong SM have to wait for all other pblocks to be launched */
@@ -36,9 +37,9 @@ int fgpu_device_init(const fgpu_dev_ctx_t *dev_ctx)
             for (int i = 0; i < blockIdx.x; i++)
                 while(!dev_ctx->d_dev_indicators->indicators[i].started);
         }
-
-        /* Note: This is the most time taking process */
-        dev_ctx->d_host_indicators->indicators[blockIdx.x].started = true;
+ 
+        /* Prepare for the next function */
+        dev_ctx->d_bindex->bindexes[dev_ctx->color].index[dev_ctx->index ^ 1] = 0;
     }
   
     if (sm < dev_ctx->start_sm || sm > dev_ctx->end_sm) {
