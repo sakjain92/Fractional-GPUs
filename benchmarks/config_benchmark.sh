8e15645789e9960fe0da453d57bc68eb9ab611fd
Saksham Jain <sakjain92@gmail.com>
2019-01-09 02:07:54 -0800

    Benchmark: Make script amendable
    Allow users to pass arguments to the script to modify the behaviour.
    
    Signed-off-by: Saksham Jain <sakjain92@gmail.com>

--- benchmarks/config_benchmark.sh
+++ benchmarks/config_benchmark.sh
@@ -1,14 +1,22 @@
+#!/bin/bash
+
 # This file contains various configurable parameters for running benchmarks
 
+#Below are default parameters that can either be changed via changing this script
+# or suuplying appropriate arguments
+
+COMMON_SCRIPT=../scripts/common.sh
+
+if [ ! -f $COMMON_SCRIPT ]; then
+    echo "Run this script from \$PROJ_DIR/benchmark folder"
+fi
+
+source $COMMON_SCRIPT
+
 # This script is used to collect statistics on benchmarks
-PROJ_PATH=$PWD/../                  # Path to FGPU project directory
-BIN_PATH=$PROJ_PATH/build           # Path to binaries (edit if different)
-SCRIPTS_PATH=$PROJ_PATH/scripts     # Folder containing scripts
 LOG_FILE=$PWD/log.txt               # File to log results
 
 ENABLE_VOLTA_MPS_PARTITION="0"      # If 1, then we use Volta MPS for QoS
-
-SERVER=fgpu_server                  # Server name
 
 NUM_ITERATION=1000                  # Number of iterations of the benchmarks
 MEMORY=1000000000                   # Amount of memory for each benchmark (About 1GB)
@@ -16,7 +24,7 @@ INTERFERENCE_COLOR=1                # Color on which to run interference app (st
 BENCHMARK_COLOR=0                   # Color on which to run benchmarks
 INTERFERENCE_PRIO=1                 # Real time priority of interference app
 BENCHMARK_PRIO=99                   # Real time priority of benchmark app
-NUM_COLORS=2		    	    # Number of interferenceing applications + 1
+NUM_COLORS=2		    	        # Number of interferenceing applications + 1
 
 # List of benchmarks to run
 benchmarks=(
@@ -26,13 +34,179 @@ benchmarks=(
 "cudaSDK_sp"        # Scalar Product
 "cudaSDK_fwt"       # Fast Walsh Transform
 "rodinia_cfd"       # Computational Fluid Dynamics
-"rodinia_nn"        # Nearest neighbours
-"rodinia_gaussian"  # Gaussian Elimination
+#"rodinia_nn"        # Nearest neighbours
+#"rodinia_gaussian"  # Gaussian Elimination
 )
 
+NO_INTERFERENCE_DUMMY_APP="__none__"
+
 # List of interference applications that run in background
-interference=(
+# For now, they are subset of the list of benchmarks are these are the most
+# interesting intereference applications
+interferences=(
+$NO_INTERFERENCE_DUMMY_APP
 "cudaSDK_mms"       # Computational Intensive
 "cudaSDK_fwt"       # Shared memory and memory intensive
 "cudaSDK_va"        # Memory Intensive
 )
+
+
+# Prefix for statement that shows that average runtime
+# Statement is only printed if there is only one benchmark and one interefence application
+# Otherwise a matrix is printed (without having the prefix)
+RESULT_PREFIX="AVG_RUNTIME:"
+
+print_usage() {
+    echo "Usage:"
+    echo "./run_benchmark.sh"
+    echo "Optional Options:"
+    echo "-b, --benchmark Select one benchmark from all available benchmarks applications"
+    echo "-B, --benchmarks Prints default benchmark applications"
+    echo "-c=<val>, --colors=<val> Sets the number of colors that hardware is configured for."
+    echo "-h, --help Shows this usage."
+    echo "-i, --interference Select one interference from all available interfence applications"
+    echo "-I, --interferences Prints default interfence applications"
+    echo "-n=<val>, --numIterations=<val> Set the number of iterations. Time show in average of runtimes of all iterations."
+    echo "-v, --voltaMPS Enables MPS compute partitioning on Volta GPU. FGPU compute/memory coloring should be disabled in this configuration."
+}
+
+# Prints all benchmark applications
+print_benchmarks() {
+    echo "List of benchmark applications:"
+    for i in ${benchmarks[@]}; do
+        echo $i
+    done
+}
+
+# Prints all interefence applications
+print_interference() {
+    echo "List of interfence applications:"
+    for i in ${interferences[@]}; do
+        echo $i
+    done
+}
+
+# The values above are the default values of the variable. Some of the variables
+# can be configured at runtime.
+parse_args() {
+
+    unset b
+    unset it
+    c=$NUM_COLORS
+    n=$NUM_ITERATION
+    v=$ENABLE_VOLTA_MPS_PARTITION
+
+    for i in "$@"
+    do
+        case $i in
+        -h*|--help*)
+            print_usage
+            exit 1
+            ;;
+
+        -I*|--interferences*)
+            # List all interfence
+            print_interference
+            exit 1
+            ;;
+
+        -B*|--benchmarks*)
+            # List all benchmarks
+            print_benchmarks
+            exit 1;
+            ;;
+
+        -b*|--benchmark=*)
+            b="${i#*=}"
+            
+            # Check if benchmark exists
+            unset found
+            for j in "${benchmarks[@]}"
+            do
+                if [ "$j" = "$b" ] ; then
+                    found=1
+                fi
+            done
+
+            if [ -z "$found" ]; then
+                echo "Invalid benchmark application"
+                print_benchmarks
+                exit 1
+            fi
+
+            shift # past argument=value
+            ;;
+        
+        -i*|--interference=*)
+            it="${i#*=}"
+            
+            # Check if benchmark exists
+            unset found
+            for j in "${interferences[@]}"
+            do
+                if [ "$j" = "$it" ] ; then
+                    found=1
+                fi
+            done
+
+            if [ -z "$found" ]; then
+                echo "Invalid interfence application"
+                print_interference
+                exit 1
+            fi
+
+            shift # past argument=value
+            ;;
+
+        -c=*|--colors=*)
+            c="${i#*=}"
+            check_arg_is_number $c
+            if [ $? -ne 0 ]; then
+                print_usage
+                exit 1
+            fi
+
+            shift # past argument=value
+            ;;
+        -n=*|--numIterations=*)
+            it="${i#*=}"
+            check_arg_is_number $it
+            if [ $? -ne 0 ]; then
+                print_usage
+                exit 1
+            fi
+
+            shift # past argument=value
+            ;;
+        -v*|--voltaMPS=*)
+            V="1"
+            shift # past argument=value
+            ;;
+        *)
+            # unknown option
+            print_usage
+            exit 1
+            ;;
+        esac
+    done
+
+    NUM_COLORS=$c
+    NUM_ITERATION=$n
+    ENABLE_VOLTA_MPS_PARTITION=$v
+    
+    if [ ! -z $b ]; then
+        benchmarks=($b)
+    fi
+    
+    if [ ! -z $it ]; then
+        interferences=($it)
+    fi
+
+    echo "Number of colors:$NUM_COLORS"
+    echo "Running benchmarks with number of iterations:$NUM_ITERATION"
+    if [ "$ENABLE_VOLTA_MPS_PARTITION" -eq "0" ]; then
+        echo "Compute Partition based on Volta MPS is stated to be OFF"
+    else
+        echo "Compute Partition based on Volta MPS is stated to be ON"
+    fi
+}
--- benchmarks/run_benchmark.sh
+++ benchmarks/run_benchmark.sh
@@ -3,17 +3,14 @@
 # Include file containing configurable options
 source ./config_benchmark.sh
 
+# Parse command line arguments
+parse_args $@
+
 # Script needs root permissions
-if [[ $EUID -ne 0 ]]; then
-    echo "This script should not be run using sudo or as the root user"
-    exit 1
-fi
+check_if_sudo
 
 #Change pwd to Proj directory 
 cd $PROJ_PATH
-
-# Remove log file 
-rm $LOG_FILE &> /dev/null
 
 touch $LOG_FILE
 TEMP_LOG_FILE=`mktemp`
@@ -24,7 +21,14 @@ proc=`nproc`
 
 # Simuntaneously running application - Benchmark and interference app
 # Divide cpus into two part
+# Each interference/benchmark app needs atleast two CPU cores since CUDA spawns
+# background threads. Hence for no inteference on CPU side, it is better to assign
+# non-overlapping CPU cores.
 app_proc=$((proc/NUM_COLORS))
+if [[ "$app_proc"  -eq "0" ]] ||  [[ "$app_proc" -eq "1" ]]; then
+    do_error_exit "Not enough CPU cores in the system to run benchmark"
+fi
+
 num_it=$((NUM_COLORS - 1))
 
 int_proc_ranges=()
@@ -39,38 +43,26 @@ bench_proc_range=$((proc - app_proc))'-'$((proc-1))
 
 AVG="0"
 
-# Kills a process 
-# First argument is the name of the process
-kill_process() {
-    pid=`pgrep $1`
-    if [ ! -z "$pid" ]
-    then
-         kill $pid &> /dev/null
-         wait $pid &> /dev/null
-    fi
-}
-
 # Kill all known processes that this script might have ran
 kill_all_processes () {
     for bench in ${benchmarks[@]}; do
         kill_process $bench
     done
    
-    for int in ${interference[@]}; do
+    for int in ${interferences[@]}; do
         kill_process $int
     done
-
-    kill_process $SERVER
 }
 
 # Incase of sigint, kill all processes (fresh state) and stop mps
 function do_for_sigint() {
     kill_all_processes
-    $SCRIPTS_PATH/mps_kill.sh  &> /dev/null
+
+    deinit_fgpu
     exit 1
 }
 
-trap 'do_for_sigint' 2
+trap 'do_for_sigint' EXIT
 
 # Function to run a benchmark with an interfering application
 # First Arg - Application to run
@@ -81,27 +73,21 @@ run_benchmark () {
     bench_app=$1
     int_app=$2
 
-    echo "Starting New benchmark: [$bench_app, $int_app]"
+    echo "Starting New Benchmark/Interference combination: [$bench_app, $int_app]"
 
     # Kill all processes
     kill_all_processes
 
-    # Start MPS
-     $SCRIPTS_PATH/mps_init.sh &> /dev/null
-
-    # Remove server file
-     rm -f /dev/shm/fgpu_shmem > /dev/null
-     rm -f /dev/shm/fgpu_host_shmem > /dev/null
-
-    sleep 5
-
-    # Start server
-    $BIN_PATH/$SERVER &> /dev/null &
-    sleep 5
+    init_fgpu
 
     percentage=`expr 100 / $NUM_COLORS`
 
-    if [ ! -z $int_app ]
+    # Run inteference applications (in a infinte loop). Wait for sometime for them to start running.
+    # Then start running the benchmark. Wait for benchmark to complete. Kill all
+    # inteference applications then (since they will not exit on their own as running in infinte loop).
+
+    # If no interference, skip launching inteference application
+    if ! [[ $int_app = $NO_INTERFERENCE_DUMMY_APP ]]
     then
         for i in `seq 1 $num_it`;
         do  
@@ -111,10 +97,10 @@ run_benchmark () {
 	        if [ $ENABLE_VOLTA_MPS_PARTITION -ne "0" ];
         	then
             		CUDA_MPS_ACTIVE_THREAD_PERCENTAGE=$percentage taskset -c $range schedtool -R -p $INTERFERENCE_PRIO -e  $BIN_PATH/$int_app -c $color -m $MEMORY  -k &> /dev/null &
-			echo "Interference:$int_app, Color:$color, Range:$range, MPS Percentage:$percentage"
+                    echo "Interference:$int_app, Color:$color, CPU Affinity:$range, MPS Percentage:$percentage"
         	else
             		taskset -c $range schedtool -R -p $INTERFERENCE_PRIO -e  $BIN_PATH/$int_app -c $color -m $MEMORY  -k &> /dev/null &
-                        echo "Interference:$int_app, Color:$color, Range:$range"
+                        echo "Interference:$int_app, Color:$color, CPU Affinity:$range"
         	fi
     	done
         sleep 5
@@ -124,10 +110,10 @@ run_benchmark () {
     if [ $ENABLE_VOLTA_MPS_PARTITION -ne "0" ];
     then
         CUDA_MPS_ACTIVE_THREAD_PERCENTAGE=$percentage taskset -c $bench_proc_range schedtool -R -p $BENCHMARK_PRIO -e  $BIN_PATH/$bench_app -c $BENCHMARK_COLOR -m $MEMORY -i $NUM_ITERATION >> $TEMP_LOG_FILE
-        echo "Benchmark: $bench_app, Color:$BENCHMARK_COLOR, Range:$bench_proc_range, MPS Percentage:$percentage"
+        echo "Benchmark: $bench_app, Color:$BENCHMARK_COLOR, CPU Affinity:$bench_proc_range, MPS Percentage:$percentage"
     else
         taskset -c $bench_proc_range schedtool -R -p $BENCHMARK_PRIO -e  $BIN_PATH/$bench_app -c $BENCHMARK_COLOR -m $MEMORY -i $NUM_ITERATION >> $TEMP_LOG_FILE
-    	echo "Benchmark:$bench_app, Color:$BENCHMARK_COLOR, Range:$bench_proc_range"
+    	echo "Benchmark:$bench_app, Color:$BENCHMARK_COLOR, CPU Affinity:$bench_proc_range"
     fi
 
     ret_code=$?
@@ -136,8 +122,7 @@ run_benchmark () {
     kill_all_processes
     sleep 2
 
-    # End MPS
-     $SCRIPTS_PATH/mps_kill.sh  &> /dev/null
+    deinit_fgpu
 
     # Append to log file
     cat $TEMP_LOG_FILE >> $LOG_FILE
@@ -146,16 +131,14 @@ run_benchmark () {
     # Check if program successful?
     if [ $ret_code -ne 0 ]
     then
-        echo "Benchmark failed. See file $TEMP_LOG_FILE"
-        #exit -1
+        do_error_exit "Benchmark failed. See file $TEMP_LOG_FILE"
     fi
 
     # Get the stats
     stats=`grep -A 1  "Kernel Stats" $TEMP_LOG_FILE`
     if [ -z "$stats" ]
     then
-        echo "Couldn't find kernel stats. See file $TEMP_LOG_FILE"
-        #exit -1
+        do_error_exit "Couldn't find kernel stats. See file $TEMP_LOG_FILE"
     fi
 
     # Extract the averge runtime
@@ -167,15 +150,14 @@ run_benchmark () {
 
 runtimes=()
 
-echo "Running benchmarks with number of iterations:$NUM_ITERATION"
-echo "ENABLE_VOLTA_MPS_PARTITION is $ENABLE_VOLTA_MPS_PARTITION"
-
 # Kill all process first 
 kill_all_processes
 
+AVG_RUNTIMES=()
+
 # Pretty print the columns - Inteference Applications
-echo -n -e "App\tNo Interference\t" > $TEMP_TIME_FILE
-for i in ${interference[@]}; do
+echo -n -e "App\t" > $TEMP_TIME_FILE
+for i in ${interferences[@]}; do
     echo -n -e "$i\t" >> $TEMP_TIME_FILE
 done
 echo "" >> $TEMP_TIME_FILE
@@ -185,24 +167,27 @@ for b in ${benchmarks[@]}; do
     
     echo -n -e "$b\t" >> $TEMP_TIME_FILE
 
-    # First run with no interference
-    run_benchmark $b ""
-    echo -n -e "$AVG\t" >> $TEMP_TIME_FILE
- 
-    for i in ${interference[@]}; do
-        run_benchmark $b $i 
+    for i in ${interferences[@]}; do
+        run_benchmark $b $i
         echo -n -e "$AVG\t" >> $TEMP_TIME_FILE
+        AVG_RUNTIMES+=("$AVG")
     done
 
     echo "" >> $TEMP_TIME_FILE
 
 done
 
-
-cat $TEMP_TIME_FILE
+# If one benchmark and one inteference application, print the result statement also
+if [ ${#AVG_RUNTIMES[@]} = "1" ]; then
+    echo "$RESULT_PREFIX ${AVG_RUNTIMES[0]}"
+else
+    cat $TEMP_TIME_FILE
+fi
 
 echo "See log file at $LOG_FILE"
 
 # Delete temp file
 rm $TEMP_LOG_FILE
 rm $TEMP_TIME_FILE
+
+exit 0
